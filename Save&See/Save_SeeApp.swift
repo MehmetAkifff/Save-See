39e3a10ddfeddb3a95fae83fae606266c29d632f
MehmetAkifff <makifergn@gmail.com>
2026-07-23 11:04:31 +0300

    Initial Commit

---
+++ Save&See/Save_SeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Save_SeeApp.swift
+//  Save&See
+//
+//  Created by Mehmet Akif ERGANİ on 23.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct Save_SeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
