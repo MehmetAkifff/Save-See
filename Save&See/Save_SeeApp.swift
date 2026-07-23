c907c3bd255d8371258898afc7869cff9e97d496
MehmetAkifff <makifergn@gmail.com>
2026-07-23 11:57:55 +0300

    Switch to NSStatusItem+NSPopover for reliable menu bar click handling

--- Save&See/Save_SeeApp.swift
+++ Save&See/Save_SeeApp.swift
@@ -1,7 +1,9 @@
 import SwiftUI
+import AppKit
 import Observation
 
 @Observable
+@MainActor
 final class SnippetStore {
     var snippets: [Snippet] = []
     private let key = "snippets"
@@ -9,8 +11,10 @@ final class SnippetStore {
     init() { load() }
 
     func add(name: String, value: String) {
-        snippets.append(Snippet(name: name.trimmingCharacters(in: .whitespaces),
-                                value: value.trimmingCharacters(in: .whitespaces)))
+        snippets.append(Snippet(
+            name: name.trimmingCharacters(in: .whitespaces),
+            value: value.trimmingCharacters(in: .whitespaces)
+        ))
         save()
     }
 
@@ -38,15 +42,45 @@ struct Snippet: Codable, Identifiable {
     var value: String
 }
 
+@MainActor
+class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+    let store = SnippetStore()
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+
+        let hc = NSHostingController(rootView: ContentView().environment(store))
+        popover = NSPopover()
+        popover.behavior = .transient
+        popover.contentViewController = hc
+
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem.button?.image = NSImage(
+            systemSymbolName: "doc.on.clipboard",
+            accessibilityDescription: "Save & See"
+        )
+        statusItem.button?.action = #selector(toggle)
+        statusItem.button?.target = self
+    }
+
+    @objc func toggle() {
+        guard let button = statusItem.button else { return }
+        if popover.isShown {
+            popover.performClose(nil)
+        } else {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            NSApp.activate(ignoringOtherApps: true)
+        }
+    }
+}
+
 @main
 struct Save_SeeApp: App {
-    @State private var store = SnippetStore()
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
-        MenuBarExtra("Save & See", systemImage: "doc.on.clipboard") {
-            ContentView()
-                .environment(store)
-        }
-        .menuBarExtraStyle(.window)
+        Settings { EmptyView() }
     }
 }
